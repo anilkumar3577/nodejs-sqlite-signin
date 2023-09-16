665cc83d26031c7613843231391b972577ce630f
PintoAdi <adityavaradarajan74@gmail.com>
2023-09-16 22:40:43 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Replace with the path to your entry file
+  output: {
+    filename: 'bundle.js', // The name of the output bundle
+    path: path.resolve(__dirname, 'dist'), // The output directory
+  },
+  // Add any loaders or plugins you need here
+};
